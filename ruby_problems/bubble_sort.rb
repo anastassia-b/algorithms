52a785efe6c242ec16668c60721d6a2b0af04a2a
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-08-04 14:31:07 -0700

    Add bubble sort with prc

---
+++ ruby_problems/bubble_sort.rb
@@ -0,0 +1,46 @@
+#Proc and no proc versions
+
+class Array
+  def bubble_sort!
+    sorted = false
+
+    until sorted
+      sorted = true
+
+      each_index do |i|
+        next if i + 1 == self.length
+        j = i + 1
+        if self[i] > self[j]
+          sorted = false
+          self[i], self[j] = self[j], self[i]
+        end
+      end
+    end
+
+    self
+  end
+
+  def bubble_sort!(&prc)
+    prc ||= Proc.new { |x, y| x <=> y }
+
+    sorted = false
+    until sorted
+      sorted = true
+
+      each_index do |i|
+        next if i + 1 == self.length
+        j = i + 1
+        if prc.call(self[i], self[j]) == 1
+          sorted = false
+          self[i], self[j] = self[j], self[i]
+        end
+      end
+    end
+
+    self
+  end
+
+  def bubble_sort(&prc)
+    self.dup.bubble_sort!(&prc)
+  end
+end

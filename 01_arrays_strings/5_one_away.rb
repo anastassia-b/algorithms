ea74bc47f0404833d911f8905229c2787dd7b8ff
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-10-02 23:00:25 -0700

    Add one away tests

---
+++ 01_arrays_strings/5_one_away.rb
@@ -0,0 +1,12 @@
+#There are 3 types of edits on strings: insert a char, remove a char,
+#and replace a char. Given 2 strings, write a function to check if they are
+#one or zero edits away.
+
+def one_away?(str1, str2)
+
+end
+
+p ("pale", "ple") == true
+p ("pales", "pale") == true
+p ("pale", "gale") == true
+p ("pale", "bake") == false

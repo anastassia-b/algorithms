6c503f7d452fac9978ba1ced2578a9d1d6fdd55a
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-10-02 22:58:20 -0700

    Add palindrome permutation tests

---
+++ 01_arrays_strings/4_palindrome_perm.rb
@@ -0,0 +1,10 @@
+# Given a string, write a function to check if it is a permutation of a palindrome.
+
+def palindrome_perm?(str)
+
+end
+
+p palindrome_perm?("tact coa") == true
+p palindrome_perm?("racarce") == true
+p palindrome_perm?("banana") == false
+#"permutation like: "taco cat", etc.

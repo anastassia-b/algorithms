c9d10861c678325c0140d32610dc6f1bc9feaa9d
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-11-24 10:12:30 -0800

    Add delete, to_s to linked list

--- 2_linked_lists/linked_list.rb
+++ 2_linked_lists/linked_list.rb
@@ -28,4 +28,39 @@ class LinkedList
   def append_to_tail(data)
     @head.append_to_tail(data)
   end
+
+  def delete(data)
+    n = @head
+
+    if n.data = data
+      @head = @head.next #shifted head to next node
+      return self
+    end
+
+    while n.next != nil
+      if (n.next.data == data)
+        n.next = n.next.next;
+        return self #head didn't change
+      end
+      n = n.next
+    end
+
+    return self;
+  end
+
+  def to_s
+    n = @head
+    until n == nil
+      print "#{n.data} "
+      n = n.next
+    end
+    puts "\n"
+  end
 end
+
+head = Node.new(1)
+list = LinkedList.new(head)
+list.append_to_tail(2)
+list.append_to_tail(3)
+list.append_to_tail(4)
+puts list

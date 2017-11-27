19ded107b13da394ac2abafab171d27f12046722
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-11-26 23:56:26 -0800

    Add linked list adt

---
+++ 2_linked_lists/linked_list.js
@@ -0,0 +1,70 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head   = null;
+    this.length = 0;
+  }
+
+  add(data) {
+    const nodeToAdd = new Node(data);
+    let nodeToCheck = this.head;
+    if(!nodeToCheck) {
+          this.head = nodeToAdd;
+          this.length++;
+    return nodeToAdd;
+        }
+    while(nodeToCheck.next) {
+          nodeToCheck = nodeToCheck.next;
+        }
+    nodeToCheck.next = nodeToAdd;
+        this.length++;
+        return nodeToAdd;
+  }
+
+  get(num) {
+      const nodeToCheck = this.head;
+      let count = 0;
+
+      if(num > this.length) return "Doesn't Exist!"
+
+      while(count < num) {
+        nodeToCheck = nodeToCheck.next;
+        count++;
+      }
+
+      return nodeToCheck;
+    }
+
+  remove(num) {
+    let nodeToCheck = this.head;
+    let count = 0;
+    let prevNode = null;
+
+    if(num > length) { return "Doesn't Exist!"; }
+
+    if(num === 0) {
+      this.head = nodeToCheck.next;
+      this.length--;
+
+      return this.head;
+    }
+
+    while(count < num) {
+      prevNode = nodeToCheck;
+      nodeToCheck = nodeToCheck.next;
+      count++;
+    }
+
+    prevNode.next = nodeToCheck.next;
+    nodeToCheck = null;
+    this.length--;
+
+    return this.head;
+  }
+}

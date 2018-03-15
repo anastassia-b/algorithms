c381f28eed4af32240c9a6170532ee29e00bacfe
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-03-14 22:52:48 -0700

    Complete undirected graph

--- graphs_project/lib/01_undirected_graph.rb
+++ graphs_project/lib/01_undirected_graph.rb
@@ -1,19 +1,37 @@
+require 'byebug'
+
 class UndirectedVertex
-  # attr_reader ...
+  attr_reader :value, :edges
 
   def initialize(value)
+    @value = value
+    @edges = []
   end
 end
 
 class UndirectedEdge
-  # attr_reader ...
+  attr_reader :vertices, :cost
 
   def initialize(vertex1, vertex2, cost = 1)
+    @vertices = [vertex1, vertex2]
+    @cost = cost
+    vertex1.edges.push(self)
+    vertex2.edges.push(self)
   end
 
   def destroy!
+    @vertices[0].edges.delete(self)
+    @vertices[1].edges.delete(self)
+    @vertices = nil
   end
 
   def other_vertex(vertex)
+    if @vertices[0] == vertex
+      @vertices[1]
+    elsif @vertices[1] == vertex
+      @vertices[0]
+    else
+      raise "vertex is at neither end"
+    end
   end
 end
